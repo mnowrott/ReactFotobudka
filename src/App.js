0a95c2ceb4a516dea4e32dbeefe4eb5a4b654bfa
mnowrot <mnowrot90@gmail.com>
2020-03-15 09:07:17 +0100

    testowo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  useHistory
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from "./Views/Home";
+import TakePhoto from "./Views/TakePhoto";
+import FirstConfig from "./Views/firstConfig";
+
+class App extends Component {
+  // const history = useHistory();
+
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/takephoto">TakePhoto</Link>
+            </li>
+            <li>
+              <Link to="/firstconfig">First Config</Link>
+            </li>
+          </ul>
+
+          <div
+            onClick={() => {
+              // history.push("/takephoto");
+            }}
+          >
+            Start
+          </div>
+          <div>Konfiguracja</div>
+
+          <Route exact path="/" component={Home} />
+          <Route path="/firstconfig" component={FirstConfig} />
+          <Route path="/takephoto" component={TakePhoto} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
